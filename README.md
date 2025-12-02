6228b0040cfc928b304e5325bb7e5d428f6178e0
ccpcn <nclicb.ccpcn-qist@nhs.net>
2025-12-02 17:25:15 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pg-test
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ccpcn/pg-test)
---
+++ index.js
@@ -0,0 +1,23 @@
+import pg from 'pg';
+
+var client = new pg.Client({
+  user: 'che_db1_user',
+  password: 'GRjQXHma6GSH6ctJjXQOWGZGxRP6eWJb',
+  database: 'che_db1',
+  port: 5432,
+  host: 'dpg-d4irugkhg0os73a5pr40-a.frankfurt-postgres.render.com',
+  ssl: true,
+});
+
+//const { Client } = require('pg');
+
+//const client = new Client({ user: 'che_db1_user', host: 'dpg-d4irugkhg0os73a5pr40-a.frankfurt-postgres.render.com', database: 'che_db1', password: 'GRjQXHma6GSH6ctJjXQOWGZGxRP6eWJb', port: '5432'});
+
+client
+  .connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+  });
