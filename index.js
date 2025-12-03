62c5461c0daaf5f4f28a79aeaff5388f3cf96420
ccpcn <nclicb.ccpcn-qist@nhs.net>
2025-12-03 10:02:40 +0000

    Fixed Secret

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ var client = new pg.Client({
 
 //const { Client } = require('pg');
 
-//const client = new Client({ user: 'che_db1_user', host: 'dpg-d4irugkhg0os73a5pr40-a.frankfurt-postgres.render.com', database: 'che_db1', password: 'GRjQXHma6GSH6ctJjXQOWGZGxRP6eWJb', port: '5432'});
-
 client
   .connect()
   .then(() => {
